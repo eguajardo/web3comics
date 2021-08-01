676d8e21e5fab8ec478d24b11185d23ff2d92150
Edwin Guajardo <edwin.guajardo@gmail.com>
2021-08-01 12:56:12 -0500

    Added project boilerplate

---
+++ hardhat.config.ts
@@ -0,0 +1,30 @@
+import { HardhatUserConfig } from "hardhat/types";
+import "@nomiclabs/hardhat-waffle";
+
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const config: HardhatUserConfig = {
+  solidity: {
+    compilers: [{ version: "0.8.3", settings: {} }],
+  },
+  networks: {
+    hardhat: {
+      forking: {
+        url: process.env.MUMBAI_NODE_URL || "https://rpc-mumbai.maticvigil.com",
+      },
+    },
+    mumbai: {
+      url: process.env.MUMBAI_NODE_URL || "https://rpc-mumbai.maticvigil.com",
+      accounts: [
+        process.env.MUMBAI_ACCOUNT_1!,
+        process.env.MUMBAI_ACCOUNT_2!,
+        process.env.MUMBAI_ACCOUNT_3!,
+        process.env.MUMBAI_ACCOUNT_4!,
+        process.env.MUMBAI_ACCOUNT_5!,
+      ],
+    },
+  },
+};
+export default config;
